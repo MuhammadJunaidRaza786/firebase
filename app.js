5e3a103c372b7ec63a1cc6d83ba76e38228bc466
Junaid Raza <88501739+MuhammadJunaidRaza786@users.noreply.github.com>
2024-07-31 02:27:45 +0500

    ..

--- app.js
+++ app.js
@@ -13,63 +13,54 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCCf1fB2-cv-nfgM_xJr-xIrApp2Gv16nI",
-  authDomain: "project-1-48fb2.firebaseapp.com",
-  projectId: "project-1-48fb2",
-  storageBucket: "project-1-48fb2.appspot.com",
-  messagingSenderId: "825926836828",
-  appId: "1:825926836828:web:e597218ef08501b5e8bc99",
-  measurementId: "G-SF44HKN9HE",
+  apiKey: "AIzaSyComKkYC9X9-Nq2zSfDQW30geYcIrt5Wpw",
+  authDomain: "authentiation-1635c.firebaseapp.com",
+  projectId: "authentiation-1635c",
+  storageBucket: "authentiation-1635c.appspot.com",
+  messagingSenderId: "910220494018",
+  appId: "1:910220494018:web:e592243b50e1fc98ede75b",
+  measurementId: "G-BP25Y7Y9NY",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-const auth = getAuth();
+const auth = getAuth(app);
 
-document.getElementById("signup_button").addEventListener('click', () => {
-  let email = document.getElementById("Sign_up_e").value;
-  console.log(email);
-  let password = document.getElementById("Sign_up_p").value;
-  console.log(password);
+let signup_button = document.getElementById("signup_button");
+
+signup_button.addEventListener("click", () => {
+  const email = document.getElementById("Sign_up_e").value;
+  const password = document.getElementById("Sign_up_p").value;
 
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      // Signed up
       const user = userCredential.user;
-      alert("You're Sucessfully Sign Up");
+      alert("You're Successfully Signed Up");
       console.log(user);
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-
-      console.log(`=====> ${errorMessage} `);
-      // ..
+      console.error(`Error ${errorCode}: ${errorMessage}`);
     });
 });
 
-document
-  .getElementById("Signin_button").addEventListener('click', () => {
-    let email = document.getElementById("Sign_in_e").value;
-    console.log(email);
-    let password = document.getElementById("Sign_in_p").value;
-    console.log(password);
-
-    // signInWithEmailAndPassword(auth, email, password)
-    //   .then((userCredential) => {
-    //     // Signed in
-    //     const user = userCredential.user;
-
-    //     alert("You're Sucessfully Log In");
-    //     console.log(user);
-    //     // ...
-    //   })
-    //   .catch((error) => {
-    //     const errorCode = error.code;
-    //     const errorMessage = error.message;
-
-    //     console.log(`===> ${errorMessage}`);
-    //   });
-  });
+let signin_button = document.getElementById("Signin_button");
+signin_button.addEventListener("click", () => {
+  const email = document.getElementById("sign_in_e").value;
+  const password = document.getElementById("sign_in_p").value;
+  console.log(email);
+  console.log(password);
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+});
